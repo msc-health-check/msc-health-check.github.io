3edb0d8e4312fc715f1bf106004442adc68c6a5c
Mairon Costa <maironmscosta@gmail.com>
2021-10-02 23:39:04 -0300

    adicionando reload da pagina a cada 6 minutos

--- js/html/accordion.js
+++ js/html/accordion.js
@@ -40,7 +40,7 @@ function getAccordion (list) {
             <h2 class="accordion-header" id="panelsStayOpen-heading-${key}">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" 
                         data-bs-target="#panelsStayOpen-collapse-${key}" aria-expanded="true" aria-controls="panelsStayOpen-collapse-${key}">
-                    ${key}
+                   Nome App: ${key}
                 </button>
             </h2>
             <div id="panelsStayOpen-collapse-${key}" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-heading-${key}">
