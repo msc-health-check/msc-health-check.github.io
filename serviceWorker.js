8396a6664ae6f5a1e93b30065129cfc4152cd2ea
Mairon Costa <maironmscosta@gmail.com>
2021-10-12 17:46:58 -0300

    adicionado imagens de icons e service worker pra PWA.

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+const staticContagemValores = "msc-health-check-v1.0"
+const assets = [
+    "/",
+    // "/index.html",
+    // "/css/style.css",
+    "/js/index.js",
+    "/js/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticContagemValores).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+// Network First, then Cache Strategy
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return caches.match(event.request)
+        })
+    )
+})
